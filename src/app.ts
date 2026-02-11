14942fe7faaccad019cf8e1c1a483b0c47cb1080
marcuswesterblom <marcus.vesterblom@gmail.com>
2026-02-11 17:00:16 +0100

    Change opening layout

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,20 @@ import { createHtml } from "./createHtml";
 const form = document.getElementById("container-search") as HTMLFormElement;
 const searchInput = document.getElementById("searchInput") as HTMLInputElement;
 const resultDiv = document.getElementById("result") as HTMLDivElement;
+const header = document.getElementById("header") as HTMLDivElement;
+
+if (resultDiv.children.length === 0) {
+        document.body.classList.add("opening");
+        header.classList.add("opening");
+    } else {
+        document.body.classList.remove("opening");
+        header.classList.remove("opening");
+    }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const city = searchInput.value.trim();
+
     if(city) {
         loadTravelGuide(city);
     }
@@ -18,6 +28,10 @@ form.addEventListener("submit", (e) => {
 
 export async function loadTravelGuide(city: string){
     resultDiv.innerHTML = "Loading...";
+
+    document.body. classList.remove("opening");
+    header.classList.remove("opening");
+    
     try {
         const weather = await fetchWeather(city);
         const country = await fetchCountry(weather.location.country);
